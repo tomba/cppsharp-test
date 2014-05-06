acfa3a64ebb62ce2e77f22f19e90586f37e33f98
Tomi Valkeinen <tomi.valkeinen@iki.fi>
2014-05-06 13:17:44 +0300

    add monodev projects

--- Gen.cs
+++ Gen.cs
@@ -8,13 +8,38 @@ namespace ConsoleApplication1
 	{
 		static void Main(string[] args)
 		{
-			ConsoleDriver.Run(new MyLib());
+			if (args.Length != 3)
+			{
+				Console.WriteLine("usage: cmd <header-path> <lib-path> <out-path>\n");
+				return;
+			}
+
+			var p1 = System.IO.Path.GetFullPath(args[0]);
+			var p2 = System.IO.Path.GetFullPath(args[1]);
+			var p3 = System.IO.Path.GetFullPath(args[2]);
+
+			Console.WriteLine("HeaderPath: {0}", p1);
+			Console.WriteLine("LibPath: {0}", p2);
+			Console.WriteLine("OutPath: {0}", p3);
+
+			ConsoleDriver.Run(new MyLib(p1, p2, p3));
 			Console.WriteLine("done");
 		}
 	}
 
 	class MyLib : ILibrary
 	{
+		string m_headerPath;
+		string m_libPath;
+		string m_outPath;
+
+		public MyLib(string headerPath, string libPath, string outPath)
+		{
+			m_headerPath = headerPath;
+			m_libPath = libPath;
+			m_outPath = outPath;
+		}
+
 		public void Setup(Driver driver)
 		{
 			var options = driver.Options;
@@ -22,12 +47,12 @@ namespace ConsoleApplication1
 			options.GeneratorKind = GeneratorKind.CSharp;
 			options.LibraryName = "native";
 
-			options.OutputDir = "./";
+			options.OutputDir = m_outPath;
 
-			options.addLibraryDirs("./");
+			options.addLibraryDirs(m_libPath);
 			options.Libraries.Add("libnative.so");
 
-			options.addIncludeDirs("./");
+			options.addIncludeDirs(m_headerPath);
 			options.Headers.Add("native.h");
 
 		}
--- Makefile
+++ Makefile
@@ -10,7 +10,7 @@ Gen.exe: libnative.so Gen.cs
 	mcs Gen.cs -lib:${LIBDIR} -r:CppSharp -r:CppSharp.AST -r:CppSharp.Generator -r:CppSharp.Runtime -r:CppSharp.Parser.CSharp
 
 native.cs: Gen.exe
-	LD_LIBRARY_PATH=${LIBDIR} MONO_PATH=${LIBDIR} mono Gen.exe
+	LD_LIBRARY_PATH=${LIBDIR} MONO_PATH=${LIBDIR} mono Gen.exe ./ ./ ./
 
 Test.exe: Test.cs native.cs
 	mcs -unsafe Test.cs native.cs
